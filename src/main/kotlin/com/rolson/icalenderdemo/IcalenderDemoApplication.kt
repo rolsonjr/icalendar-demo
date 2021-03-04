2536318f3595675989c953986178cacc183a1c60
Rodney Olson <rodneyolson1@gmail.com>
2021-03-03 20:46:58 -0800

    init commit

---
+++ src/main/kotlin/com/rolson/icalenderdemo/IcalenderDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.rolson.icalenderdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IcalenderDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<IcalenderDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/rolson/icalenderdemo/IcalenderDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rolson.icalenderdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IcalenderDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
