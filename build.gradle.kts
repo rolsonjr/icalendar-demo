a56988a5c3ea588671958abb69ac8845889768ba
Rodney Olson <rodneyolson1@gmail.com>
2021-03-11 09:13:55 -0800

    some tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("net.sf.biweekly:biweekly:0.6.6")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/kotlin/com/rolson/icalenderdemo/controllers/CalendarController.kt
@@ -0,0 +1,9 @@
+package com.rolson.icalenderdemo.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/invite")
+class CalendarController {
+}
